2def57c8b7d6d75bcaf0b87e42ae5cc2f600fd4c
Nick Allison <99363282+nickrallison@users.noreply.github.com>
2022-06-07 11:19:06 -0600

    Got buttons working

--- src/Timer.js
+++ src/Timer.js
@@ -6,7 +6,7 @@ export default class Timer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            seconds: parseInt(props.startTimeInSeconds, 10) || 0,
+            seconds: parseInt(props.startTimeInSeconds, 1) || 0,
             posx: props.posx,
             theta: props.theta,
             delposx: props.delposx,
@@ -23,16 +23,17 @@ export default class Timer extends React.Component {
 
     update = () => {
 
-        let dt = 0.01
+        let dt = 0.001
+        let fr = 0.1
 
-        let deldelx = 20 * (this.state.len*this.state.pendmass*Math.sin(this.state.theta)*this.state.deltheta*this.state.deltheta + this.state.F + 9.81*this.state.pendmass*Math.cos(this.state.theta)*Math.sin(this.state.theta))/(- this.state.pendmass*Math.cos(this.state.theta)*Math.cos(this.state.theta) + this.state.cartmass + this.state.pendmass)
-        let deldeltheta = (-this.state.deltheta + Math.cos(this.state.theta) * this.state.W-this.state.deltheta * 0.01 - this.state.len*this.state.pendmass*Math.cos(this.state.theta)*Math.sin(this.state.theta)*this.state.deltheta*this.state.deltheta + this.state.F*Math.cos(this.state.theta) + 9.81*this.state.pendmass*Math.sin(this.state.theta) + this.state.cartmass*9.81*Math.sin(this.state.theta))/(this.state.len*(- this.state.pendmass*Math.cos(this.state.theta)*Math.cos(this.state.theta) + this.state.cartmass + this.state.pendmass))
+        let deldelx = (- this.state.len*this.state.pendmass*Math.sin(this.state.theta)*this.state.deltheta*this.state.deltheta - fr * this.state.pendmass * this.state.deltheta * Math.cos(this.state.theta) + this.state.W*this.state.pendmass*Math.cos(this.state.theta) * Math.cos(this.state.theta) + 9.81*this.state.pendmass*Math.sin(this.state.theta)*Math.cos(this.state.theta) + this.state.F)/(- this.state.pendmass*Math.cos(this.state.theta)*Math.cos(this.state.theta) + this.state.cartmass + this.state.pendmass)
+        let deldeltheta = (- this.state.len*this.state.pendmass*Math.cos(this.state.theta)*Math.sin(this.state.theta)*this.state.deltheta*this.state.deltheta + this.state.F*Math.cos(this.state.theta) + 9.81*this.state.pendmass*Math.sin(this.state.theta) - this.state.cartmass * fr*this.state.deltheta - this.state.pendmass * fr*this.state.deltheta+ this.state.cartmass*this.state.W*Math.cos(this.state.theta) + this.state.cartmass*9.81*Math.sin(this.state.theta) + this.state.W*this.state.pendmass*Math.cos(this.state.theta))/(this.state.len*(- this.state.pendmass*Math.cos(this.state.theta)*Math.cos(this.state.theta) + this.state.cartmass + this.state.pendmass))
 
 
-        let xnext = this.state.posx + dt * this.state.delposx
-        let thetanext = this.state.theta + dt * this.state.deltheta
-        let delxnext = this.state.delposx + dt * deldelx
-        let delthetanext = this.state.deltheta + dt * deldeltheta
+        let xnext = (this.state.posx + dt * 10 * 5 * this.state.delposx)
+        let thetanext = this.state.theta + dt * 5 * this.state.deltheta
+        let delxnext = this.state.delposx + dt * 5 * deldelx
+        let delthetanext = this.state.deltheta + dt * 5 * deldeltheta
 
 
         this.setState(state => ({
@@ -51,14 +52,32 @@ export default class Timer extends React.Component {
             deltheta: delthetanext
         }));
 
+        if (this.state.theta < 1.57 || this.state.theta > -1.57) {
+            this.setState(state => ({
+               F: -50 * this.state.theta
+            }));
+        }
+        else {
+            this.setState(state => ({
+                F: 0
+            }));
+        }
+
+
+        if (this.state.theta >  Math.PI) {
+            this.setState(state => ({
+                theta: this.state.theta - 2 * Math.PI
+            }));
+        }
+
+        if (this.state.theta < -Math.PI) {
+            this.setState(state => ({
+                theta: this.state.theta + 2 * Math.PI
+            }));
+        }
+
         this.setState(state => ({
-            F: -50 * this.state.theta
-        }));
-
-
-
-        this.setState(state => ({
-            testvar: this.state.W
+            testvar: this.state.F
         }));
 
         if (this.state.posx >  window.innerWidth / 2 + 100) {
@@ -77,13 +96,13 @@ export default class Timer extends React.Component {
 
     tick() {
         this.setState(state => ({
-            seconds: state.seconds + 0.016
+            seconds: state.seconds + 0.001
         }));
         this.update()
     }
 
     componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 16);
+        this.interval = setInterval(() => this.tick(), 1);
     }
 
     componentWillUnmount() {
@@ -112,6 +131,8 @@ export default class Timer extends React.Component {
                             <div id="pivot"></div>
                             <div id="ball"></div>
                         </div>
+                        <div className="wheel"></div>
+                        <div id="wheel2"></div>
                     </div>
 
                     <button className="button button1" onClick={() => this.setState(state => ({
